c8a7ee66dba09fa071254e29375838d2e636d371
Ankit Singh <ankit.singh@globant.com>
2019-12-04 16:59:04 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-java -cp "lib/*:bin:" org.testng.TestNG /home/ankits/eclipse-workspace/DemoTestNG/bin/DemoTestNG.xml
+Command for ubuntu--> java -cp "lib/*:bin:" org.testng.TestNG /home/ankits/eclipse-workspace/DemoTestNG/bin/DemoTestNG.xml
+Windows-->ava –cp bin;lib/* org.testng.TestNG testng.xml
+
