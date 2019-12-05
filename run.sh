5e733b4ef39c74e31316a8892ea8d61acd8391e1
Ankit Singh <ankit.singh@globant.com>
2019-12-05 11:36:26 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 Command for ubuntu--> java -cp "lib/*:bin:" org.testng.TestNG /home/ankits/eclipse-workspace/DemoTestNG/bin/DemoTestNG.xml
-Windows-->ava –cp bin;lib/* org.testng.TestNG testng.xml
+Windows-->java -cp "lib\*;bin" org.testng.TestNG D:\Project\Glow_New_Framework\IST000-automation\resources\suites\finalTest.xml
 
